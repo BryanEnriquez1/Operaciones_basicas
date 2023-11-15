ce1f1899cadc82aa9663f903084cb1a612ed6b2e
Bryan Enriquez <bryan.enriquez487@ist17dejulio.edu.ec>
2023-11-15 10:44:39 -0500

    APP OPERACION SUMA

--- app/src/main/java/com/baec/operaciones_basicas/MainActivity.java
+++ app/src/main/java/com/baec/operaciones_basicas/MainActivity.java
@@ -3,12 +3,27 @@ package com.baec.operaciones_basicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edNumero1, edNumero2;
+    TextView tvResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edNumero1=findViewById(R.id.txtnumero1);
+        edNumero2=findViewById(R.id.txtnumero2);
+        tvResultado=findViewById(R.id.lblresultado);
+    }
+
+    public void sumar_numeros(View view){
+        int num1=Integer.parseInt(edNumero1.getText().toString());
+        int num2=Integer.parseInt(edNumero2.getText().toString());
+        int resultado=num1+num2;
+        tvResultado.setText(resultado+"");
     }
 }
